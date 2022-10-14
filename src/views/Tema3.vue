995268eee9c56946be311905d2bfe03490bdc194
giorgio1966 <jehaylock@misena.edu.co>
2022-10-14 08:09:49 -0500

    Video vinculado
    Video vinculado

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -265,7 +265,7 @@
     p.mb-5(data-aos="fade-down-right") Los siguientes aspectos son importantes a considerar:
     figure(data-aos="fade")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/UgqnP2wcLFQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 </template>
 
 <script>
